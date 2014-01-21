fff1cb70d8b673b8ff4fbfa1134528a0571faa47
j-i-l <jonas.i.liechti@gmail.com>
2014-01-21 16:39:13 +0100

    minor changes

--- cryptsypy/CryptsyStatus.py
+++ cryptsypy/CryptsyStatus.py
@@ -30,24 +30,6 @@ class Status():
             if curs not in self.Pairs.keys():
                 self.Pairs[curs] = {}
         return 0
-    
-    def get_Clicks(self,):
-        self.Clicks = {}
-        Pairs = self.Pairs.keys()
-        for i in xrange(len(Pairs) - 2):
-            for j in xrange(1+i, len(Pairs) - 1):
-                for k in xrange(j+1, len(Pairs)):
-                    if any([cur in Pairs[i] for cur in Pairs[j]]):
-                        cand = filter(lambda x:x not in Pairs[i],Pairs[j])[0]
-                        middle = filter(lambda x:x != cand,Pairs[j])[0]
-                        other = filter(lambda x:x not in Pairs[j], Pairs[i])[0]
-                        if cand in Pairs[k]:
-                            o_cand = filter(lambda x:x not in Pairs[j], Pairs[k])[0]
-                            if o_cand in Pairs[i] and o_cand == other:
-                                forw = [(other, middle),(middle, cand), (cand, other)]
-                                bwd = [(middle, other),(other, cand),(cand, middle)]
-                                self.Clicks[(Pairs[i],Pairs[j],Pairs[k])] = [forw, bwd]
-        return 0
         
     def update_PricesLast(self,):
         self.PricesLast = {}
